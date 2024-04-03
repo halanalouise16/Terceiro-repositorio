de30564122451e3aa22351d875cab5c7b61a8092
lana <108102607+halanalouise16@users.noreply.github.com>
2024-04-02 22:47:19 -0300

    aula5

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
 const botoes = document.querySelectora11(".botao");
+const botoes = document.querySelectora11(".aba-conteudo");
 
 for (let i=0; i < botoes,length i++){
     botoes[i]. onclick = function() {
 
        for(let j=0; j <botoes.length; j++) {
         botoes[j].classlist.remover("ativo");
+        texto[j].classlist.remover("ativo");
        }
+        botoes[i].classlist.add("ativo");
         botoes[i].classlist.add("ativo");
     }
 }
+const contadores = document.querySelectorAll(".contador");
+
+contadores[0].textContent = "contagem regressiva");
+const tempoObjetivo = new Date("2024-05-10T23:59:59");
+let tempoAtual = new Date();
+
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
